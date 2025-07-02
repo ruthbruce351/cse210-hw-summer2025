0d6612f5334a97bde7d998745deb2b41ac965cb5
Ruth Blake <ruthblake@Ruths-Air.lan>
2025-07-02 14:30:41 -0500

    Completed Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+                if (guess == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher!");
+                }
+                else
+                {
+                    Console.WriteLine("Lower!");
+                }
+        } while (guess != magicNumber);
     }
 }
