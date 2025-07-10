76e17ab892276060e1944638a04a83adafae5de6
Ruth Blake <ruthblake@Ruths-Air.lan>
2025-07-09 19:32:43 -0500

    Complete through step 5

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer"; 
+        job1._startYear = 2015;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+
+        job1.Display();
+        job2.Display();
     }
 }
