35d306458a9fcd2aa952792a98a79c2eacb7b692
Ruth Blake <ruthbruce351@gmail.com>
2025-07-16 19:51:40 -0500

    Stubs

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+}
