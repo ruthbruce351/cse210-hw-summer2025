ed1ff344d7c2d5532b29d0e59755c50a903236b5
Ruth Blake <ruthbruce351@gmail.com>
2025-07-16 19:59:00 -0500

    Word Update

--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -5,23 +5,27 @@ public class Word
 
     public Word(string text)
     {
+        _text = text;
+        _isHidden = false;
     }
 
     public void Hide()
     {
+        _isHidden = true;
     }
 
     public void Show()
     {
+        _isHidden = false;
     }
 
     public bool IsHidden()
     {
-        return false;
+        return _isHidden;
     }
 
     public string GetDisplayText()
     {
-        return "";
+        return _isHidden ? new string('_',_text.Length) : _text;
     }
 }
