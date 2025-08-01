4beadb502bd6dd846953676ab716532e11d36db9
Ruth Blake <ruthbruce351@gmail.com>
2025-07-31 21:26:54 -0500

    Finished Program, but something is not right. Need to go back tomorrow and figure out what is missing.

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("STREET", "CITY", "STATE", "COUNTRY");
+        Customer customer1 = new Customer("Ruth Blake", address1);
+        Order order1 = new Order(customer1);
+
+        order1.AddProduct(new Product("Test1", 20.99, 2));
+        order1.AddProduct(new Product("Test2", 1.99, 1));
+
+        DisplayOrder(order1);
+
+        static void DisplayOrder(Order order)
+        {
+            Console.WriteLine(order.GetPackingLabel());
+            Console.WriteLine(order.GetShippingLabel());
+            Console.WriteLine($"Total Cost: ${order.GetTotalCost()}");
+        }
     }
 }
