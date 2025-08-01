70ac26f556aeec628a0527d2e50020a3828de5d6
Ruth Blake <ruthbruce351@gmail.com>
2025-07-31 20:45:11 -0500

    Finished Video class

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -5,11 +5,21 @@ class Video
     public int _length;
     public List<Comment> _comments;
 
-    public void DisplayVideo (string title, string author, int length)
+    public void DisplayVideo(string title, string author, int length)
     {
         _title = title;
         _author = author;
         _length = length;
         _comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+    
+    public int NumberOfComments()
+    {
+        return _comments.Count;
     }
 }
