2d6b1a2248b0832cd2ca63f8e41d82f2bc582f44
Ruth Blake <ruthbruce351@gmail.com>
2025-07-31 21:05:52 -0500

    Product Class

---
+++ week04/OnlineOrdering/Product.cs
@@ -0,0 +1,30 @@
+public class Product
+{
+    private string _productName;
+    private string _productID;
+    private double _price;
+    private int _quantity;
+
+    public Product(string productName, string productID, double price, int quantity)
+    {
+        _productName = productName;
+        _productID = productID;
+        _price = price;
+        _quantity = quantity;
+    }
+
+    public double GetTotalCost()
+    {
+        return _price * _quantity;
+    }
+
+    public string GetProductName()
+    {
+        return _productName;
+    }
+
+    public string GetProductID()
+    {
+        return _productID;
+    }
+}
