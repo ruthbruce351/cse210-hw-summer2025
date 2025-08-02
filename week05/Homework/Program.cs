d08aa2038660dc2d241643110056ca0e8036d6e6
Ruth Blake <ruthbruce351@gmail.com>
2025-08-02 10:56:11 -0500

    Parent Class test

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Test1 Test1", "Class 1");
+        Assignment assignment2 = new Assignment("Test2 Test2", "Class 2");
+
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(assignment2.GetSummary());
+
     }
 }
