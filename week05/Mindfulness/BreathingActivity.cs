f4f40daf758e39e2fab3bdd09ed8a5e2fcffac1c
Ruth Blake <ruthbruce351@gmail.com>
2025-08-02 11:44:34 -0500

    Breathing Activity stubs

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,13 @@
+public class BreathingActivity : Activity
+{
+    public BreathingActivity(string activityName, string description, string duration)
+        : base(activityName, description, duration)
+    {
+
+    }
+
+    public void Run()
+    {
+        
+    }
+}
