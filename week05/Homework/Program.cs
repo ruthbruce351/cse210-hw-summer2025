a30e843c4e4dec8f0aeda714f77c775285ea9937
Ruth Blake <ruthbruce351@gmail.com>
2025-08-02 11:02:42 -0500

    Test Math Assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,11 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Assignment assignment1 = new Assignment("Test1 Test1", "Class 1");
-        Assignment assignment2 = new Assignment("Test2 Test2", "Class 2");
-
-        Console.WriteLine(assignment1.GetSummary());
-        Console.WriteLine(assignment2.GetSummary());
-
+        MathAssignment mathAssignment = new MathAssignment("Test1 Test1", "Test Subject"," 5.1", "1-10");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
     }
 }
