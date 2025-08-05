924641ef02b549d88c6de37fe9b2eb68fa729d35
Ruth Blake <ruthbruce351@gmail.com>
2025-08-05 16:48:02 -0500

    Activity class

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,11 @@
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+}
