2b09bbea284a9b42d0ebe8d9d566e684a4a8576c
Ruth Blake <ruthbruce351@gmail.com>
2025-08-04 19:53:47 -0500

    Program written

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager goalManager = new GoalManager();
+        goalManager.Start();
+
+        bool running = true;
+        while (running)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Create New Goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Save Goals");
+            Console.WriteLine("4. Load Goals");
+            Console.WriteLine("5. Record Event");
+            Console.WriteLine("6.Quit");
+
+            Console.Write("Select a choice from the menu: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    goalManager.CreateGoal();
+                    break;
+                case "2":
+                    goalManager.ListGoalNames();
+                    break;
+                case "3":
+                    goalManager.SaveGoals();
+                    break;
+                case "4":
+                    goalManager.LoadGoals();
+                    break;
+                case "5":
+                    goalManager.RecordEvent();
+                    break;
+                case "6":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
 }
