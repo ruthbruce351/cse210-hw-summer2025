cd7f68fc4aa37fc6750726e50eb6e5922935c886
Ruth Blake <ruthbruce351@gmail.com>
2025-08-04 19:36:23 -0500

    Fixed abstract method format

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -11,15 +11,11 @@ public abstract class Goal
         _points = points;
     }
 
-    public abstract void RecordEvent()
-    { }
+    public abstract void RecordEvent();
 
-    public abstract bool IsComplete()
-    { }
+    public abstract bool IsComplete();
 
-    public abstract string GetDetailsString()
-    { }
+    public abstract string GetDetailsString();
 
-    public abstract string GetStringRepresentation()
-    { }
+    public abstract string GetStringRepresentation();
 }
